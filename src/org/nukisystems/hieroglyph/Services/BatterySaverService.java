05c8198d1abd240a7da003329d94349df967a0f1
hiroshi. <hiroshi@tanuki.page>
2026-09-12 02:27:08 -0400

    BatterySaverService: Null guard onStartCommand intent

--- src/org/nukisystems/hieroglyph/Services/BatterySaverService.java
+++ src/org/nukisystems/hieroglyph/Services/BatterySaverService.java
@@ -54,7 +54,7 @@ public class BatterySaverService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if ("org.nukisystems.hieroglyph.UPDATE_BATTERY_SAVER".equals(intent.getAction())){
+        if (intent.getAction() != null && "org.nukisystems.hieroglyph.UPDATE_BATTERY_SAVER".equals(intent.getAction())){
             trackBatterySaver = intent.getBooleanExtra("status", false);
         }
         updateStatus();
