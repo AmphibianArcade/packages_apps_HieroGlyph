5a51e819af701a929f4985693ef3b7956c52bdf0
hiroshi. <hiroshi@tanuki.page>
2026-09-20 09:15:36 -0400

    ServiceUtils: Drop extra start call for ThirdPartyService

--- src/org/nukisystems/hieroglyph/Utils/ServiceUtils.java
+++ src/org/nukisystems/hieroglyph/Utils/ServiceUtils.java
@@ -227,7 +227,6 @@ public final class ServiceUtils {
 
         if (SettingsManager.getGlyphBrightness() != Constants.getBrightness()) {
             Constants.setBrightness(SettingsManager.getGlyphBrightness());
-            startThirdPartyService();
             // if (StatusManager.isEssentialLedActive()) AnimationManager.playEssential();
         }
         
